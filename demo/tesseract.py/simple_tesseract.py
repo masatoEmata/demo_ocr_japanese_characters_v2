8d9a8cb785e477b6c8d122d99686bcd426ceada2
masatoEmata <lh57.nm@gmail.com>
2022-05-14 15:23:34 +0900

    tesseract

---
+++ demo/tesseract.py/simple_tesseract.py
@@ -0,0 +1,15 @@
+import cv2
+from pytesseract import pytesseract
+from pytesseract import Output
+import re
+import glob
+
+pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+img_paths = glob.glob('../../data/zip/*.png')
+for path in img_paths:
+    img = cv2.imread(path)
+    img_data = pytesseract.image_to_data(img, output_type=Output.DICT)
+    words = img_data['text']
+    print(path)
+    print(words)
